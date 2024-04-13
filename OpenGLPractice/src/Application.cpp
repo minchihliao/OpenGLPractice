5ef731808c9b70ebc3da2e9a954cc5d934970b5f
minchihliao <xicoo0730@gmail.com>
2024-04-13 19:13:27 +0800

    Added Textures

--- OpenGLPractice/src/Application.cpp
+++ OpenGLPractice/src/Application.cpp
@@ -9,6 +9,7 @@
 #include "VertexArray.h"
 #include "Shader.h"
 #include "VertexBufferLayout.h"
+#include "Texture.h"
 
 int main(void)
 {
@@ -43,10 +44,10 @@ int main(void)
 
     {
         float positions[] = {
-            -0.5f,-0.5f,  //0
-             0.5f,-0.5f,  //1
-             0.5f, 0.5f,  //2
-            -0.5f, 0.5f   //3
+            -0.5f,-0.5f, 0.0f, 0.0f,  //0
+             0.5f,-0.5f, 1.0f, 0.0f, //1
+             0.5f, 0.5f, 1.0f, 1.0f, //2
+            -0.5f, 0.5f, 0.0f, 1.0f   //3
         };
 
         unsigned int indices[] = {
@@ -54,12 +55,14 @@ int main(void)
             2, 3, 0
         };
 
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
         VertexArray va;
-
-
-        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
   
         VertexBufferLayout layout;
+        layout.Push<float>(2);
         layout.Push<float>(2);
         va.AddBuffer(vb,layout);
   
@@ -89,6 +92,9 @@ int main(void)
 
             shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
+            Texture texture("res/textures/enemy.png");
+            texture.Bind();
+            shader.SetUniform1i("u_Texture", 0);
 
             renderer.Draw(va, ib, shader);
 
--- OpenGLPractice/src/Shader.h
+++ OpenGLPractice/src/Shader.h
@@ -19,16 +19,18 @@ public:
 	void Unbind() const;
 
 	//Set Uniforms
+	void SetUniform1i(const std::string& name,int value);
+	void SetUniform1f(const std::string& name, float value);
 	void SetUniform4f(const std::string& name,float v0, float v1, float v2, float v3);
 private:
-	unsigned int GetUniformLocation(const std::string& name);
+	int GetUniformLocation(const std::string& name);
 	unsigned int CompileShader(unsigned int type, const std::string& source);
 	ShaderProgramSource ParseShader(const std::string& filepath);
 	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
 private:
 	unsigned int m_RendererID;
 	std::string m_FilePath;
-	std::unordered_map<std::string, unsigned int> m_UniformLocationCache;
+	std::unordered_map<std::string, int> m_UniformLocationCache;
 	// caching for uniforms
 
 };
