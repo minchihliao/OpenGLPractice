2e49ea5bef171a3736ff99047fe45008f4370071
minchihliao <xicoo0730@gmail.com>
2024-04-20 21:40:44 +0800

    Added ImGUi library.

--- OpenGLPractice/src/Application.cpp
+++ OpenGLPractice/src/Application.cpp
@@ -11,109 +11,168 @@
 #include "VertexBufferLayout.h"
 #include "Texture.h"
 
+#include "glm/glm.hpp";
+#include "glm/gtc/matrix_transform.hpp";
+
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_glfw.h"
+#include <ImGui/imgui_impl_opengl3.h>
+
+
 int main(void)
 {
-    GLFWwindow* window;
+	GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    glfwSwapInterval(1);
-
-    if (glewInit() != GLEW_OK)
-        std::cout << "Error!" << std::endl;
-
-    std::cout << glGetString(GL_VERSION) << std::endl;
-
-    {
-        float positions[] = {
-            -0.5f,-0.5f, 0.0f, 0.0f,  //0
-             0.5f,-0.5f, 1.0f, 0.0f, //1
-             0.5f, 0.5f, 1.0f, 1.0f, //2
-            -0.5f, 0.5f, 0.0f, 1.0f   //3
-        };
-
-        unsigned int indices[] = {
-            0, 1, 2,
-            2, 3, 0
-        };
-
-        GLCall(glEnable(GL_BLEND));
-        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-        VertexArray va;
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-  
-        VertexBufferLayout layout;
-        layout.Push<float>(2);
-        layout.Push<float>(2);
-        va.AddBuffer(vb,layout);
-  
-        IndexBuffer ib(indices, 6);
-   
-        Shader shader("res/shaders/Basic.shader");
-        shader.Bind();
-        shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-   
-        va.Unbind();
-        vb.UnBind();
-        ib.UnBind();
-        shader.Unbind();
-
-        Renderer renderer;
-
-        float r = 0.0f;
-        float increment = 0.05f;
-        /* Loop until the user closes the window */
-        while (!glfwWindowShouldClose(window))
-        {
-            /* Render here */
-            renderer.Clear();
+	const char* glsl_version = "#version 130";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 
-            shader.Bind();
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-            shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
 
-            Texture texture("res/textures/enemy.png");
-            texture.Bind();
-            shader.SetUniform1i("u_Texture", 0);
+	glfwSwapInterval(1);
 
-            renderer.Draw(va, ib, shader);
+	if (glewInit() != GLEW_OK)
+		std::cout << "Error!" << std::endl;
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	{
+		float positions[] = {
+			100.0f, 100.0f, 0.0f, 0.0f,  //0
+			200.0f, 100.0f, 1.0f, 0.0f, //1
+			200.0f, 200.0f, 1.0f, 1.0f, //2
+			100.0f, 200.0f, 0.0f, 1.0f   //3
+		};
+
+		unsigned int indices[] = {
+			0, 1, 2,
+			2, 3, 0
+		};
+
+		GLCall(glEnable(GL_BLEND));
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+		VertexArray va;
+		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+
+		VertexBufferLayout layout;
+		layout.Push<float>(2);
+		layout.Push<float>(2);
+		va.AddBuffer(vb, layout);
+
+		IndexBuffer ib(indices, 6);
+
+		//Projection Matrix
+		glm::mat4 proj = glm::ortho(0.f, 960.f, 0.f, 540.0f, -1.0f, 1.0f);
+		// View Matrix
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+		glm::mat4 mvp = proj * view * model;
+
+		Shader shader("res/shaders/Basic.shader");
+		shader.Bind();
+		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+		shader.SetUniformMat4F("u_MVP", mvp);
+
+		va.Unbind();
+		vb.UnBind();
+		ib.UnBind();
+		shader.Unbind();
+
+		Renderer renderer;
+		ImGui::CreateContext();
+		ImGuiIO& io = ImGui::GetIO(); (void)io;
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		ImGui_ImplOpenGL3_Init(glsl_version);
+		ImGui::StyleColorsDark();
+
+		float r = 0.0f;
+		float increment = 0.05f;
+		/* Loop until the user closes the window */
+		while (!glfwWindowShouldClose(window))
+		{
+			/* Render here */
+			renderer.Clear();
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
 
 
-            if (r > 1.0f)
-                increment = -0.05f;
-            else if (r < 0.0f)
-                increment = 0.05f;
-            r += increment;
+			shader.Bind();
 
-            /* Swap front and back buffers */
-            GLCall(glfwSwapBuffers(window));
+			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
-            /* Poll for and process events */
-            GLCall(glfwPollEvents());
-        }
+			Texture texture("res/textures/enemy.png");
+			texture.Bind();
+			shader.SetUniform1i("u_Texture", 0);
 
-        // Delete Here
-    }
-    glfwTerminate();
-    return 0;
+			renderer.Draw(va, ib, shader);
+
+			bool show_demo_window = true;
+			bool show_another_window = false;
+			ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+			if (r > 1.0f)
+				increment = -0.05f;
+			else if (r < 0.0f)
+				increment = 0.05f;
+			r += increment;
+
+			{
+				static float f = 0.0f;
+				static int counter = 0;
+
+				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+				ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+				ImGui::Checkbox("Another Window", &show_another_window);
+
+				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+				if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+					counter++;
+				ImGui::SameLine();
+				ImGui::Text("counter = %d", counter);
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+				ImGui::End();
+			}
+
+
+
+			ImGui::Render();
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+			/* Swap front and back buffers */
+			GLCall(glfwSwapBuffers(window));
+
+			/* Poll for and process events */
+			GLCall(glfwPollEvents());
+		}
+
+		// Delete Here
+	}
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+	glfwTerminate();
+	return 0;
 }
--- OpenGLPractice/src/Shader.h
+++ OpenGLPractice/src/Shader.h
@@ -2,6 +2,8 @@
 #include <string>
 #include <unordered_map>
 
+#include "glm/glm.hpp"
+
 struct ShaderProgramSource
 {
 	std::string VertexSource;
@@ -22,6 +24,7 @@ public:
 	void SetUniform1i(const std::string& name,int value);
 	void SetUniform1f(const std::string& name, float value);
 	void SetUniform4f(const std::string& name,float v0, float v1, float v2, float v3);
+	void SetUniformMat4F(const std::string& name,const glm::mat4& matrix);
 private:
 	int GetUniformLocation(const std::string& name);
 	unsigned int CompileShader(unsigned int type, const std::string& source);
