f0c7c7eb436b4e6f740b38168d3dd473110aa783
minchihliao <xicoo0730@gmail.com>
2024-04-20 21:54:00 +0800

    Use SliderFloat3 to control our texture

--- OpenGLPractice/src/Application.cpp
+++ OpenGLPractice/src/Application.cpp
@@ -87,7 +87,7 @@ int main(void)
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-		shader.SetUniformMat4F("u_MVP", mvp);
+
 
 		va.Unbind();
 		vb.UnBind();
@@ -103,6 +103,8 @@ int main(void)
 		ImGui_ImplOpenGL3_Init(glsl_version);
 		ImGui::StyleColorsDark();
 
+		glm::vec3 translation(200, 200, 0);
+
 		float r = 0.0f;
 		float increment = 0.05f;
 		/* Loop until the user closes the window */
@@ -117,17 +119,18 @@ int main(void)
 
 			shader.Bind();
 
+			glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+			glm::mat4 mvp = proj * view * model;
+
+
 			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
 			Texture texture("res/textures/enemy.png");
 			texture.Bind();
 			shader.SetUniform1i("u_Texture", 0);
-
+			shader.SetUniformMat4F("u_MVP", mvp);
 			renderer.Draw(va, ib, shader);
 
-			bool show_demo_window = true;
-			bool show_another_window = false;
-			ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 			if (r > 1.0f)
 				increment = -0.05f;
@@ -136,25 +139,8 @@ int main(void)
 			r += increment;
 
 			{
-				static float f = 0.0f;
-				static int counter = 0;
-
-				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-				ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-				ImGui::Checkbox("Another Window", &show_another_window);
-
-				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-				if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-					counter++;
-				ImGui::SameLine();
-				ImGui::Text("counter = %d", counter);
-
+				ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
 				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-				ImGui::End();
 			}
 
 
